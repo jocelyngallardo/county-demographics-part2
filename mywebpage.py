ac1e3a4bface207b5f377d02ca42530d6606d15d
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-11-01 09:42:48 -0700

    Update mywebpage.py

--- mywebpage.py
+++ mywebpage.py
@@ -2,4 +2,9 @@ from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
 
-def fun_fact(counties)
+def main():
+    with open('county_demographics.json') as demographics_data:
+        states = json.load(demographics_data)
+
+def fun_fact(states):
+  state = {}
