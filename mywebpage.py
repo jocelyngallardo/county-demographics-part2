cfdca5c666b8691a8530f6db347663cf9f311f32
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-11-04 10:01:02 -0800

    updated

--- mywebpage.py
+++ mywebpage.py
@@ -5,10 +5,18 @@ import json
 app = Flask(__name__)
 
 @app.route("/")
+
+def get_state_options(states):
+    listOfStates = []
+    for data in states:
+        if (data['State'] not in listOfStates):
+            listOfStates.append(data['State'])
+    return listOfStates
+
 def render_main():
     with open('county_demographics.json') as demographics_data:
         states = json.load(demographics_data)
-    return render_template('home.html')
+    return render_template('home.html', states=listOfStatess)
 
 if __name__=="__main__":
     app.run(debug=False)
